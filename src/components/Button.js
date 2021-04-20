dbac1ef224ee00af8cc7692cea7c45de1bdc106a
maurya512 <patelmaurya0512@gmail.com>
2021-04-19 23:10:59 -0400

    Fixed an error that caused the buttons to default on the styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -13,11 +13,11 @@ export const Button = ({ children, type, onClick, buttonStyle, buttonSize, butto
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
      
     // checks if a pre disposed style is applied to the button and if not apply the style from the size's index position
-    const checkButtonSize = STYLES.includes(buttonSize) ? buttonSize : SIZES[0]
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
 
     // checks if a pre disposed color for the button is available
-    const checkButtonColor = STYLES.includes(buttonColor) ? 
-    buttonColor: COLOR[0]
+    const checkButtonColor = COLOR.includes(buttonColor) ? 
+    buttonColor: null;
 
     return (
         <button className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonColor}`} onClick={onClick} type={type}>{children}</button>
